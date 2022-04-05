8a424a0ed98dc74cd1d68be4beacd4d67aa6e654
Pedro Lima <contact.pedrolima@gmail.com>
2022-04-05 17:51:58 -0300

    a

--- src/components/components/SliderData.js
+++ src/components/components/SliderData.js
@@ -8,6 +8,7 @@ export const SliderData = [
                 name='Space Invaders'
                 image='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/space%20invaders.png'
                 image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste.png'
+                image2='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste1.png'
                 wid='400px' hei='500px'
                 description="A game based on the classic Space Invaders. The player has to destroy all the enemies before they reach the bottom of the screen. Created with pygame and based in the freeCodeCamp&apos;s tutorial."
                 gitlink='https://github.com/phenriquep00/phenriquep00/tree/main/space_invader'
@@ -18,7 +19,8 @@ export const SliderData = [
             <Project
                 name='Hangman Game'
                 image='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/hangman.png'
-                image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste.png'
+                image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/hangman.png'
+                image2='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/hangman.png'
                 wid='400px' hei='500px'
                 description="A Hangman game developed in React.Js. The game is based on the classic game of hangman. The player has to guess the word before the stickman if completely drawn in the hang"
                 gitlink='https://github.com/phenriquep00/hangman'
@@ -29,7 +31,8 @@ export const SliderData = [
             <Project
                 name='CantinaCord'
                 image='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/cantina-cord.png'
-                image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste.png'
+                image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/cantina-cord.png'
+                image2='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/cantina-cord.png'
                 wid='400px' hei='500px'
                 description="A discord-based app created during Alura&apos;s React Immersion. This app was created only with the purpose of learning the basics of React.Js and the discord API."
                 gitlink='https://github.com/phenriquep00/cantina-cord'
@@ -41,6 +44,7 @@ export const SliderData = [
                 name='Imersão Dados Alura'
                 image='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/imersao%20dados.png'
                 image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste.png'
+                image2='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste1.png'
                 wid='400px' hei='500px'
                 description="Data Science Project developet during Alura&apos;s Data Immersion. With this project, I was able to learn how to use the data science tools and how to use them to solve problems."
                 gitlink='https://github.com/phenriquep00/imersao_dados_alura'
@@ -52,6 +56,7 @@ export const SliderData = [
                 name='Poker Game'
                 image='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/poker.png'
                 image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste.png'
+                image2='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste1.png'
                 wid='400px' hei='500px'
                 description="A Poker game developed with python and the pygame library. It&apos;s single player and it&apos;s a game of chance. The player has to choose wisely in the right order to win."
                 gitlink='https://github.com/phenriquep00/poker'
@@ -100,6 +105,8 @@ function Project(props) {
                                 if (image === props.image) {
                                     setImage(props.image1)
                                 } else if (image === props.image1) {
+                                    setImage(props.image2)
+                                } else if (image === props.image2) {
                                     setImage(props.image)
                                 }
                             }
@@ -128,6 +135,8 @@ function Project(props) {
                                         if (image === props.image) {
                                             setImage(props.image1)
                                         } else if (image === props.image1) {
+                                            setImage(props.image2)
+                                        } else if (image === props.image2) {
                                             setImage(props.image)
                                         }
                                     }
