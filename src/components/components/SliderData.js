501f91a7d28f3e3096b09b5860f646c16c2b6d05
Pedro Lima <contact.pedrolima@gmail.com>
2022-04-05 17:07:17 -0300

    multiple images inside projects

--- src/components/components/SliderData.js
+++ src/components/components/SliderData.js
@@ -7,6 +7,7 @@ export const SliderData = [
             <Project
                 name='Imersão Dados Alura'
                 image='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/imersao%20dados.png'
+                image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste.png'
                 wid='400px' hei='500px'
                 description="Data Science Project developet during Alura&apos;s Data Immersion. With this project, I was able to learn how to use the data science tools and how to use them to solve problems."
                 gitlink='https://github.com/phenriquep00/imersao_dados_alura'
@@ -17,6 +18,7 @@ export const SliderData = [
             <Project
                 name='Hangman Game'
                 image='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/hangman.png'
+                image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste.png'
                 wid='400px' hei='500px'
                 description="A Hangman game developed in React.Js. The game is based on the classic game of hangman. The player has to guess the word before the stickman if completely drawn in the hang"
                 gitlink='https://github.com/phenriquep00/hangman'
@@ -27,6 +29,7 @@ export const SliderData = [
             <Project
                 name='CantinaCord'
                 image='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/cantina-cord.png'
+                image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste.png'
                 wid='400px' hei='500px'
                 description="A discord-based app created during Alura&apos;s React Immersion. This app was created only with the purpose of learning the basics of React.Js and the discord API."
                 gitlink='https://github.com/phenriquep00/cantina-cord'
@@ -37,6 +40,7 @@ export const SliderData = [
             <Project
                 name='Space Invaders'
                 image='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/space%20invaders.png'
+                image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste.png'
                 wid='400px' hei='500px'
                 description="A game based on the classic Space Invaders. The player has to destroy all the enemies before they reach the bottom of the screen. Created with pygame and based in the freeCodeCamp&apos;s tutorial."
                 gitlink='https://github.com/phenriquep00/phenriquep00/tree/main/space_invader'
@@ -47,6 +51,7 @@ export const SliderData = [
             <Project
                 name='Poker Game'
                 image='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/poker.png'
+                image1='https://raw.githubusercontent.com/phenriquep00/portifolio/master/src/components/img/projects/teste.png'
                 wid='400px' hei='500px'
                 description="A Poker game developed with python and the pygame library. It&apos;s single player and it&apos;s a game of chance. The player has to choose wisely in the right order to win."
                 gitlink='https://github.com/phenriquep00/poker'
@@ -58,6 +63,7 @@ function Project(props) {
     const [bgFocus, setBgFocus] = React.useState('#5F66A3')
     const [colorFocus, setColorFocus] = React.useState('#ffffff')
     const [borderFocus, setBorderFocus] = React.useState('1px solid #ffffff')
+    const [image, setImage] = React.useState(props.image)
 
     return (
         <>
@@ -82,9 +88,43 @@ function Project(props) {
                     }}
                 >
                     {/* img div */}
-                    <div>
-                        <img style={{ width: props.wid, hei: props.hei, margin: '35px', border: '3px solid #ffffff' }} src={props.image} />
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}
+                        onClick={() => {
+                            {
+                                if (image === props.image) {
+                                    setImage(props.image1)
+                                } else {
+                                    setImage(props.image)
+                                }
+                            }
+                        }}
+                    >
+                        <img style={{ width: props.wid, hei: props.hei, margin: '35px', border: '3px solid #ffffff' }} src={image} />
+                        {
+                            setTimeout(() => {
+                                if (image === props.image) {
+                                    setImage(props.image1)
+                                } else {
+                                    setImage(props.image)
+                                }
+                            }, 10000)
+                        }
+                        <div
+                            style={{
+                                width: '50px',
+                                height: '50px',
+                                backgroundColor: '#243035',
+                                marginTop: '-50px',
+                            }}
                         
+                        >
+
+                        </div>
+
                     </div>
                     {/* text div */}
                     <div style={{ display: 'flex', flexDirection: 'column', margin: '5px', fontSize: '22px', marginTop: '30px', color: '#ffffff', wordSpacing: '8px', textAlign: 'justify', marginRight: '20px', lineHeight: '30px' }}>
@@ -124,4 +164,4 @@ function Project(props) {
             </div>
         </>
     )
-}  
+}
