cb28ec8efa41158a67ff7f572895c9a9af176f43
Pedro Lima <contact.pedrolima@gmail.com>
2022-05-10 17:06:19 -0300

    created projects tab structure

--- src/components/Content/Projects/Projects.js
+++ src/components/Content/Projects/Projects.js
@@ -1,7 +1,20 @@
-export function Projects () {
+import { Options } from "./Options/RadioGroup";
+
+
+export function Projects() {
     return (
-        <div>
-            Projects
+        <div className="flex flex-row w-full h-full">
+
+            <div className="p-4 w-[23%] h-[100%]"> {/* Options div */}
+                <Options />
+            </div>
+            
+            <div className="p-4 w-[81%] h-[100%] border-2 border-orange-600"> {/* focused div */}
+
+            </div>
+
         </div>
+            
+        
     )
 }
