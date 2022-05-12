603df9208c952a666cc54a8850a56d8d111cfcd6
Pedro Lima <contact.pedrolima@gmail.com>
2022-05-12 18:09:15 -0300

    style review

--- src/components/Content/Projects/Overview/ImageSlider/NavigationButton.js
+++ src/components/Content/Projects/Overview/ImageSlider/NavigationButton.js
@@ -4,13 +4,13 @@ export function NavigationButton({ direction, gotClicked }) {
             {
                 direction === "left"
                     ?
-                    (<div className="bg-gradient-to-r from-neutral-600 to-transparent  h-[30%] rounded-full flex items-center ">
+                    (<div className="bg-transparent  h-[30%] rounded-full flex items-center ">
                         <button onClick={gotClicked} type="button" className="">
                             <img src="https://img.icons8.com/ios/50/000000/left-squared.png" alt="left Arrow" className="opacity-40 hover:opacity-100"/>
                         </button>
                     </div>)
                     :
-                    (<div className="ml-[80%] bg-gradient-to-l from-neutral-600 to-transparent h-[30%] rounded-full flex items-center ">
+                    (<div className="ml-[80%] bg-transparent h-[30%] rounded-full flex items-center ">
                         <button onClick={gotClicked}>
                             <img src="https://img.icons8.com/ios/50/000000/right-squared.png" alt="right Arrow" className="opacity-40 hover:opacity-100"/>
                         </button>
