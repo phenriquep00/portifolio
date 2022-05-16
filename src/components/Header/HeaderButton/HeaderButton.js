c791a9b9c47d6439d26b7c80a6978ea7568662c9
Pedro Lima <contact.pedrolima@gmail.com>
2022-05-16 15:25:39 -0300

    changed line height to fix animation issues

--- src/components/Header/HeaderButton/HeaderButton.js
+++ src/components/Header/HeaderButton/HeaderButton.js
@@ -3,7 +3,7 @@ export function HeaderButton({ text, onClickToFocus, focus }) {
     return (
         <div
             onClick={onClickToFocus}
-            className={`text-cyan-100 antialiased font-mono flex-1 md:py-4 py-2  font-bold hover:text-xl transition-all transform-400  hover:cursor-pointer ${focus ? 'bg-slate-800 z-50' : 'bg-gradient-to-t from-indigo-600 to-slate-500 opacity-50 hover:opacity-100'} rounded-t-lg text-center`}>
+            className={`text-cyan-100 antialiased font-mono flex-1 md:py-4 py-2 leading-8 font-bold hover:text-xl transition-all transform-200  hover:cursor-pointer ${focus ? 'bg-slate-800 z-50' : 'bg-gradient-to-t from-indigo-600 to-slate-500 opacity-50 hover:opacity-100'} rounded-t-lg text-center`}>
             {text}
         </div>
 
