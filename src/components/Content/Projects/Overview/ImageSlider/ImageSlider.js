e0f6407cf10ab451f9ba535833e5c63910a0b44d
Pedro Lima <contact.pedrolima@gmail.com>
2022-05-17 15:10:02 -0300

    added nlw images

--- src/components/Content/Projects/Overview/ImageSlider/ImageSlider.js
+++ src/components/Content/Projects/Overview/ImageSlider/ImageSlider.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { NavigationButton } from './NavigationButton';
 
+import nlw1 from '../../../../assets/img/nlw/nlw1.png';
+import nlw2 from '../../../../assets/img/nlw/nlw2.png';
+import nlw3 from '../../../../assets/img/nlw/nlw3.png';
+
+
 import cat1 from '../../../../assets/cat1.jpg'
 import cat2 from '../../../../assets/cat2.jpg'
 import cat3 from '../../../../assets/cat3.jpg'
@@ -13,10 +18,6 @@ import mice1 from '../../../../assets/mice1.jpg'
 import mice2 from '../../../../assets/mice2.jpeg'
 import mice3 from '../../../../assets/mice3.jpg'
 
-import fish1 from '../../../../assets/fish1.jpg'
-import fish2 from '../../../../assets/fish2.jpg'
-import fish3 from '../../../../assets/fish3.jpg'
-
 import bird1 from '../../../../assets/bird1.jpg'
 import bird2 from '../../../../assets/bird2.jpg'
 import bird3 from '../../../../assets/bird3.jpg'
@@ -25,9 +26,9 @@ import bird3 from '../../../../assets/bird3.jpg'
 
 export function ImageSlider({ project }) {
 
-    const [image1, setImage1] = useState(fish1);
-    const [image2, setImage2] = useState(fish2);
-    const [image3, setImage3] = useState(fish3);
+    const [image1, setImage1] = useState(nlw1);
+    const [image2, setImage2] = useState(nlw2);
+    const [image3, setImage3] = useState(nlw3);
 
 
     const [currentImage, setCurrentImage] = React.useState(image1);
@@ -47,46 +48,50 @@ export function ImageSlider({ project }) {
             setImage1(cat1);
             setImage2(cat2);
             setImage3(cat3);
+            setCurrentImage(image1);
         }
-        else if (project === "Imersao Dados Alura") {
+        else if (project === "Imersão Dados Alura") {
             setImage1(dog1);
             setImage2(dog2);
             setImage3(dog3);
+            setCurrentImage(image1);
         }
         else if (project === "Pong") {
             setImage1(mice1);
             setImage2(mice2);
             setImage3(mice3);
+            setCurrentImage(image1);
         }
         else if (project === "NLW return") {
-            setImage1(fish1);
-            setImage2(fish2);
-            setImage3(fish3);
+            setImage1(nlw1);
+            setImage2(nlw2);
+            setImage3(nlw3);
+            setCurrentImage(image1);
         }
         else if (project === "Hangman") {
             setImage1(bird1);
             setImage2(bird2);
             setImage3(bird3);
+            setCurrentImage(image1);
         }
         else if (project === "Space Invaders") {
             setImage1(bird1);
             setImage2(bird2);
             setImage3(bird3);
+            setCurrentImage(image1);
         } else if (project === "React Alura") {
             setImage1(bird1);
             setImage2(bird2);
             setImage3(bird3);
+            setCurrentImage(image1);
         }
 
-    }, [project]);
+    }, [project, image1, image2, image3]);
 
-    useEffect(() => {
-        setCurrentImage(image1);
-    }, [image1, image2, image3]);
 
     return (
         <div
-            className="m-4 w-11/12 h-full p-6 text-justify shadow-2xl drop-shadow-2xl rounded-md ring-1 ring-gray-900 flex flex-row items-center transition-all duration-500 ease-in-out transform hover:scale-105"
+            className="m-4 w-11/12 h-full p-6 text-justify shadow-2xl drop-shadow-2xl rounded-md ring-1 ring-gray-900 flex flex-row items-center transition-all duration-100 ease-in-out transform hover:scale-105"
             style={{
                 backgroundImage: `url(${currentImage})`,
                 backgroundSize: 'cover',
