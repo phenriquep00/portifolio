59bb01403127b8e0fc46dd18f7a3e078c786bd47
Pedro Lima <contact.pedrolima@gmail.com>
2022-05-18 17:27:54 -0300

    stylized contacts

--- src/components/Content/Contact/SocialButtons.js
+++ src/components/Content/Contact/SocialButtons.js
@@ -5,7 +5,7 @@ export function SocialButtons({ image, link }) {
     }
 
     return (
-        <div className={`self-center w-12 h-12 bg-teal-800 rounded-full flex items-center p-2`}>
+        <div className={`self-center w-12 h-12 bg-violet-700 rounded-full flex items-center p-2 ring-2 ring-violet-500 hover:ring-cyan-100 ring-offset-cyan-300 ring-offset-1 shadow-2xl drop-shadow-2xl`}>
             <button onClick={handleClick}>
                 <img src={image} alt="logo" />
             </button>
